b78dcc8bfb1fd1975f5ba362de75f9b3be58802e
niagao@in66 <niangao@in66.com>
2018-05-24 16:53:27 +0800

    hias

---
+++ char/Hias.h
@@ -0,0 +1,13 @@
+//
+//  Hias.h
+//  char
+//
+//  Created by Lanmaq on 2018/5/24.
+//  Copyright © 2018年 Lanmaq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hias : NSObject
+
+@end
